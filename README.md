c9ab5c99f71f04768784e09da4197176b4a7289d
ihab-younis <ihab.younis86@gmail.com>
2024-11-12 14:56:58 -0600

    Adding one endpoint to be called in the CustomerConsumerApplication, adding the readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Customer Service
+## Description
+    Providing Consumner for the customer service
+
+### Framework
+Spring Boot is used to create this microservice, yeeay! ;) <br>
+
+### Language
+English is what i used.. kidding XD .. <br>
+JAVA 17 is the programming language, some groovy is used implicitly though by gradle
+
+### Build
+Gradle is the packaging and builder tool for this microservice. <br>
+To build, you can run "./gradlew clean build" on the project directory
+
+### Run
+port : 8081<br>
+Run "./gradlew bootRun" on the project directory<br>
+OR pull it to IDE e.g. intelli (developed with) and run CustomerConsumerApplication.java<br>
+It will call one endpoint http://localhost:8080/customer/v1/all while the booting and logging the result <br>
--- src/main/java/com/ihab/interview/client/CustomerConsumerApplication.java
+++ src/main/java/com/ihab/interview/client/CustomerConsumerApplication.java
@@ -1,13 +1,25 @@
 package com.ihab.interview.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CustomerConsumerApplication {
+    static Logger logger = LoggerFactory.getLogger(CustomerConsumerApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(CustomerConsumerApplication.class, args);
+
+        String allCustomersJson = restTemplate().getForObject("http://localhost:8080/customer/v1/all", String.class);
+        logger.info("All Customers response : {}", allCustomersJson);
     }
 
+    @Bean
+    public static RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 }
